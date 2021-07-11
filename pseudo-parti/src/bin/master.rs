5aff8b0f63c32b723e0009eee740d717e3a79c3f
fulara <ntszar@gmail.com>
2021-07-11 17:41:05 +0900

    master now infinitely waits for new connection.

--- pseudo-parti/src/bin/master.rs
+++ pseudo-parti/src/bin/master.rs
@@ -1,6 +1,6 @@
-use pseudo_parti::{say_hello, setup_signal_handler};
+use pseudo_parti::setup_signal_handler;
 use std::net::{SocketAddr, TcpListener, TcpStream};
-use std::sync::atomic::AtomicBool;
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::thread;
 use structopt::StructOpt;
@@ -13,21 +13,30 @@ struct Options {
 
 fn slave(_stream: TcpStream) {}
 
+fn accept_incoming_forever(listener: TcpListener, interrupted: &AtomicBool) -> anyhow::Result<()> {
+    let mut counter = 0usize;
+    while !interrupted.load(Ordering::Relaxed) {
+        let name = format!("c_{}", counter);
+        let (stream, source) = listener.accept()?;
+        println!("Connection:{} accepted from: {:?}", name, source);
+        thread::Builder::new()
+            .name(name)
+            .spawn(|| {
+                slave(stream);
+            })
+            .unwrap();
+        counter += 0;
+    }
+    Ok(())
+}
+
 fn main() -> anyhow::Result<()> {
     let opts = Options::from_args();
     let interrupted = Arc::new(AtomicBool::new(false));
-    setup_signal_handler(interrupted)?;
+    setup_signal_handler(interrupted.clone())?;
     let address = SocketAddr::from(([127, 0, 0, 1], opts.port));
     let listener = TcpListener::bind(&address)?;
     println!("Now listening on port: {}", opts.port);
-    let (stream, source) = listener.accept()?;
-    println!("Connection accepted from: {:?}", source);
-    thread::Builder::new()
-        .name("c_0".into())
-        .spawn(|| {
-            slave(stream);
-        })
-        .unwrap();
-    println!("master says {} opts: {:?}", say_hello(), opts);
+    accept_incoming_forever(listener, &interrupted)?;
     Ok(())
 }
