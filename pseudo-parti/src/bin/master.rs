5d93f1f1a5638cf40934b4466dc03713b56be77f
fulara <ntszar@gmail.com>
2021-07-11 17:04:12 +0900

    added Server and Listener to the parti.

--- pseudo-parti/src/bin/master.rs
+++ pseudo-parti/src/bin/master.rs
@@ -1,4 +1,6 @@
 use pseudo_parti::say_hello;
+use std::net::{SocketAddr, TcpListener, TcpStream};
+use std::thread;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -7,7 +9,21 @@ struct Options {
     port: u16,
 }
 
-fn main() {
+fn slave(_stream: TcpStream) {}
+
+fn main() -> anyhow::Result<()> {
     let opts = Options::from_args();
+    let address = SocketAddr::from(([127, 0, 0, 1], opts.port));
+    let listener = TcpListener::bind(&address)?;
+    println!("Now listening on port: {}", opts.port);
+    let (stream, source) = listener.accept()?;
+    println!("Connection accepted from: {:?}", source);
+    thread::Builder::new()
+        .name("c_0".into())
+        .spawn(|| {
+            slave(stream);
+        })
+        .unwrap();
     println!("master says {} opts: {:?}", say_hello(), opts);
+    Ok(())
 }
--- pseudo-parti/src/bin/slave.rs
+++ pseudo-parti/src/bin/slave.rs
@@ -1,5 +1,5 @@
 use pseudo_parti::say_hello;
-use std::net::SocketAddr;
+use std::net::{SocketAddr, TcpStream};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -8,7 +8,11 @@ struct Options {
     host: SocketAddr,
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let opts = Options::from_args();
+    println!("Slave connecting to master on: {:?}", opts.host);
+    TcpStream::connect(opts.host)?;
+    println!("Connection established.");
     println!("slave says {} {:?}", say_hello(), opts);
+    Ok(())
 }
